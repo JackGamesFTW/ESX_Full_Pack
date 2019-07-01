464d3e67eb14a83911ad77a97a3166ecc56b289a
jackgamesftw <jackswain873@gmail.com>
2019-07-01 12:17:52 +0100

    Added the Addons

---
+++ README.md
@@ -0,0 +1,5 @@
+# ESX Full Pack #
+
+All the configs and sql files are in english.
+
+Enjoy!
---
+++ esx_basicneeds/esx_basicneeds.sql
@@ -0,0 +1,6 @@
+USE `essentialmode`;
+
+INSERT INTO `items` (`name`, `label`, `limit`) VALUES
+	('bread', 'Bread', 10),
+	('water', 'Water', 5)
+;
